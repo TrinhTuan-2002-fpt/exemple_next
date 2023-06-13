d36ac7c19088b073f85aa4886533d3233069d719
TrinhTuan-2002-fpt <tuantu2k2badaoyl@gmail.com>
2023-06-13 09:30:06 +0700

    layout and page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 import React from "react";
 
 const page = () => {
+  // throw new Error("Not today!!");
   return (
-    <>
+    <div className="flex flex-col">
       <h2>about </h2>
       <Link href={"/"} replace>
         go to home page
       </Link>
-    </>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex">
+    <main className="flex flex-col">
       <p>check</p>
+      <Link href={"/about"}>go to about page</Link>
     </main>
   );
 }
